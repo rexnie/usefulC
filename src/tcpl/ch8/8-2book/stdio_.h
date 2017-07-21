39fde8ab08934effa64ffa3d81817dbd0f9fb1a7
niedaocai <niedaocai@snda.com>
2017-07-21 15:53:19 +0800

    add 8-2book

---
+++ src/tcpl/ch8/8-2book/stdio_.h
@@ -0,0 +1,47 @@
+#ifndef __STDIO__H__
+#define __STDIO__H__
+
+#undef NULL
+#define NULL 	 0
+#define EOF 	 (-1)
+#define BUFSIZ   (1024)
+#define OPEN_MAX (20)
+
+typedef struct _iobuf {
+	int cnt; 	/* buf中剩余字符个数 */
+	char *ptr; 	/* 指向buf的下一个读写位置 */
+	char *base; 	/* buf地址 */
+	int flag; 	/* 文件访问模式 */
+	int fd; 	/* unix 底层fd */
+} FILE;
+
+extern FILE _iob[OPEN_MAX];
+
+#define stdin 	(&_iob[0])
+#define stdout 	(&_iob[1])
+#define stderr 	(&_iob[2])
+
+enum _flags {
+	_READ  	= 01,
+	_WRITE  = 02,
+	_UNBUF 	= 04,
+	_EOF 	= 010,
+	_ERR 	= 020
+};
+
+int _fillbuf(FILE *);
+int _flushbuf(int, FILE*);
+
+#define feof(p) 	(((p)->flag & _EOF) != 0)
+#define ferror(p) 	(((P)->flag & _ERR) != 0)
+#define fileno(p) 	((p)->fd)
+
+#define getc(p) 	(--(p)->cnt >= 0 \
+			? (unsigned char) *(p)->ptr++ : _fillbuf(p))
+
+#define putc(x, p) 	(--(p)->cnt >= 0 \
+			? *(p)->ptr++ = (x) : _flushbuf((x), (p)))
+
+#define getchar() 	getc(stdin)
+#define putchar(x) 	putc((x), stdout)
+#endif
