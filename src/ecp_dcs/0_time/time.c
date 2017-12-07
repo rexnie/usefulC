7fd529fcc53441b8f0edf0966a3d90ef218e96b6
rex <niedao516@126.com>
2017-12-07 10:20:21 +0800

    add find_max_time_t_value function

--- src/ecp_dcs/0_time/time.c
+++ src/ecp_dcs/0_time/time.c
@@ -3,7 +3,40 @@
 #include <limits.h>
 
 #define ONE_DAY (24 * 60 * 60)
-#define TM_FMT "%Y/%m/%d %H:%M:%S"
+#define TM_FMT "%Y/%m/%d %H:%M:%S\n"
+#define LINE_MAX 1024
+
+time_t find_max_valid_time(void)
+{
+	time_t low = 0, high = LONG_MAX;
+	time_t mid;
+	struct tm* tm_mid;
+	char *p;
+	char buf[LINE_MAX];
+
+	do {
+		mid = (low + high) / 2;
+		tm_mid = gmtime(&mid);
+		if(tm_mid == NULL) {
+			high = mid;
+		} else {
+			low = mid;
+		}
+
+		dbg("mid=%ld, high-low=%ld, tm_mid=%p\n", mid, high - low, tm_mid);
+	} while((high - low > 1) || (tm_mid == NULL));
+
+	dbg("mid=%ld, high-low=%ld, tm_mid=%p\n", mid, high - low, tm_mid);
+
+	strftime(buf, LINE_MAX, TM_FMT, tm_mid);
+	dbg("max value utc time = %s", buf);
+
+	/* asctime return null, but tm_mid is not null
+	 * the max value gmtime & asctime can express is implement-depend
+	 * */
+	dbg("max value, utc time=%s\n", asctime(tm_mid));
+	return mid;
+}
 
 int main(void)
 {
@@ -11,32 +44,28 @@ int main(void)
 	time_t diff;
 	double diff_d;
 	struct tm *local, *utc;
-	char buf[1024];
+	char buf[LINE_MAX];
 	char *p;
 
-	t2 = LONG_MAX;
-	p = NULL;
-	while(p == NULL) {
-		t2 -= ONE_DAY;
-		p = ctime(&t2);
-		dbg("%ld: %s\n", t2, p);
-	}
+	dbg("%ld, %ld\n", sizeof(time_t), sizeof(long));
+
+	t2 = find_max_valid_time();
 
 	time(&t1);
-	dbg("local time=%s", ctime(&t1));
-	dbg("utc time=%s", asctime(gmtime(&t1)));
+	dbg("now local time=%s", ctime(&t1));
+	dbg("now utc time=%s", asctime(gmtime(&t1)));
 
-	diff_d = difftime(t1, t2);
+	diff_d = difftime(t2, t1);
 	diff = (time_t) diff_d;
+	dbg("diff_d = %f, diff = %ld\n", diff_d, diff);
+
 	local = localtime(&diff);
+	strftime(buf, LINE_MAX, TM_FMT, local);
+	dbg("diff local time = %s", buf);
 	utc = gmtime(&diff);
 
-	strftime(buf, 1024, TM_FMT, local);
-	dbg("diff_d = %f, diff = %ld\n", diff_d, diff);
-	dbg("local time = %s", buf);
-
-	strftime(buf, 1024, TM_FMT, utc);
-	dbg("utc time = %s", buf);
+	strftime(buf, LINE_MAX, TM_FMT, utc);
+	dbg("diff utc time = %s", buf);
 
 	return 0;
 }
