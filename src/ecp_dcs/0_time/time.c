27cf6df5fde6c6f2d52a95967e9abd339de725c6
rex <niedao516@126.com>
2018-05-05 09:43:26 +0800

    note ch0 time issue

--- src/ecp_dcs/0_time/time.c
+++ src/ecp_dcs/0_time/time.c
@@ -2,70 +2,48 @@
 #include <time.h>
 #include <limits.h>
 
-#define ONE_DAY (24 * 60 * 60)
-#define TM_FMT "%Y/%m/%d %H:%M:%S\n"
-#define LINE_MAX 1024
-
-time_t find_max_valid_time(void)
+time_t get_max_time_t(void)
 {
-	time_t low = 0, high = LONG_MAX;
-	time_t mid;
-	struct tm* tm_mid;
-	char *p;
-	char buf[LINE_MAX];
+	printf("sizeof(int)=%lu sizeof(long)=%lu sizeof(time_t)=%lu\n", sizeof(int),
+		sizeof(long), sizeof(time_t));
 
-	do {
-		mid = (low + high) / 2;
-		tm_mid = gmtime(&mid);
-		if(tm_mid == NULL) {
-			high = mid;
-		} else {
-			low = mid;
-		}
-
-		dbg("mid=%ld, high-low=%ld, tm_mid=%p\n", mid, high - low, tm_mid);
-	} while((high - low > 1) || (tm_mid == NULL));
-
-	dbg("mid=%ld, high-low=%ld, tm_mid=%p\n", mid, high - low, tm_mid);
-
-	strftime(buf, LINE_MAX, TM_FMT, tm_mid);
-	dbg("max value utc time = %s", buf);
-
-	/* asctime return null, but tm_mid is not null
-	 * the max value gmtime & asctime can express is implement-depend
-	 * */
-	dbg("max value, utc time=%s\n", asctime(tm_mid));
-	return mid;
+	/**
+	 * TODO: check time_t data type under new platform
+	 * time_t define in /usr/include/time.h
+	 * intel i5-5287U iOS10.12
+	 *     time_t: long int
+	 * VM ubuntu 12.04 64bit
+	 *     time_t: long int
+	 */
+	/* 如果使用LONG_MAX的话，则会导致ctime()函数返回NULL */
+	/*return LONG_MAX;*/
+	return 0x7fffffff;
 }
 
 int main(void)
 {
-	time_t t1, t2;
+	time_t max, now;
 	time_t diff;
 	double diff_d;
 	struct tm *local, *utc;
-	char buf[LINE_MAX];
-	char *p;
 
-	dbg("%ld, %ld\n", sizeof(time_t), sizeof(long));
+	max = get_max_time_t();
+	dbg("max=%lx\n", max);
+	dbg("max local time=%s", ctime(&max));
+	dbg("max utc time=%s", asctime(gmtime(&max)));
 
-	t2 = find_max_valid_time();
+	time(&now);
+	dbg("now local time=%s", ctime(&now));
+	dbg("now utc time=%s", asctime(gmtime(&now)));
 
-	time(&t1);
-	dbg("now local time=%s", ctime(&t1));
-	dbg("now utc time=%s", asctime(gmtime(&t1)));
-
-	diff_d = difftime(t2, t1);
+	diff_d = difftime(max, now);
 	diff = (time_t) diff_d;
-	dbg("diff_d = %f, diff = %ld\n", diff_d, diff);
-
 	local = localtime(&diff);
-	strftime(buf, LINE_MAX, TM_FMT, local);
-	dbg("diff local time = %s", buf);
 	utc = gmtime(&diff);
 
-	strftime(buf, LINE_MAX, TM_FMT, utc);
-	dbg("diff utc time = %s", buf);
+	dbg("diff_d = %f, diff = %ld\n", diff_d, diff);
+	dbg("diff time(local)=%s", asctime(local));
+	dbg("diff time(utc)=%s", asctime(utc));
 
 	return 0;
 }
