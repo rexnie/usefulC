942005bb96c7adb83a7870548c4f6a68317773a6
Daocai Nie <niedaocai@huaqin.corp-partner.google.com>
2022-07-31 08:05:22 +0800

    [linux_binary_analysis][string_table.c] add test result about string

--- src/linux_binary_analysis/string_table.c
+++ src/linux_binary_analysis/string_table.c
@@ -1,9 +1,40 @@
 #include<stdio.h>
 #include<malloc.h>
 
+/** test on gcc 9.4
+strings in .strtab section:
+	string_table.c
+	global_int_var_inited
+	global_string_name
+	global_int_var_uninited
+	global_func
+	local_static.1799
+	malloc@@GLIBC_2.2.5
+	printf@@GLIBC_2.2.5
+	puts@@GLIBC_2.2.5
+
+strings in .dynstr section:
+	puts
+	printf
+	malloc
+
+strings in .rodata section:
+	daocain
+	in global func...
+	Hello World
+	malloc failed
+	%s %d %d\n
+*/
+
 char *global_string_name = "daocain";
 int global_int_var_inited = 11;
 int global_int_var_uninited;
+
+int global_func(void)
+{
+	printf("in global func...\n");
+	return 0;
+}
 
 int main(int argc,char *argv[])
 {
@@ -18,7 +49,8 @@ int main(int argc,char *argv[])
 		printf("malloc failed\n");
 		return -1;
 	}
-	printf("%s\n", local_string_a);
+	local_int_d = 3;
+	printf("%s %d %d\n", local_string_a, local_int_d, global_func());
 	return 0;
 }
 
